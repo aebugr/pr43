41dcc0becfcd7b63d0406a4bf76710ef0a6dd7ed
BugrinaA <tsaaeel@gmail.com>
2024-05-30 01:06:13 +0500

    Реализация MainWindow + структура проекта + картинки. Бугрина

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,12 @@ namespace ManagingTasksCategoriesBugrina
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static MainWindow init;
         public MainWindow()
         {
             InitializeComponent();
+            init = this;
+            DataContext = new ViewModels.VM_Pages();
         }
     }
 }
